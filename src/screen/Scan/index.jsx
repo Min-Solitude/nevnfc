208c061af0e71f90d1b5fcc8bc8480a6b5218b03
Min solitude <121474260+Min-Solitude@users.noreply.github.com>
2023-08-30 13:30:35 +0700

    1330

--- src/screen/Scan/index.jsx
+++ src/screen/Scan/index.jsx
@@ -2,71 +2,54 @@ import React, { useState } from 'react'
 import Section from '../../motion/Section'
 import QrReader from 'react-qr-scanner'
 import Swal from 'sweetalert2';
-import CameraPhoto, { FacingMode } from 'react-html5-camera-photo'; 
-
 
 const Scan = () => {
 
     const [scanSuccess, setScanSuccess] = useState(false);
 
-    // const handleScan = async (data) => {
-    //     if(data?.text) {
-    //         setScanSuccess(true);
+    const handleScan = async (data) => {
+        if(data?.text) {
+            setScanSuccess(true);
 
-    //         try {
-    //             Swal.fire(
-    //                 `Vé hợp lệ! ${data.text}`,
-    //                 '',
-    //                 'success'
-    //             ).then(e => setScanSuccess(false));
-    //         }catch(e) {
-    //             console.log(e);
-    //             Swal.fire(
-    //                 'Vé không hợp lệ!',
-    //                 '',
-    //                 'error'
-    //             ).then(e => setScanSuccess(false));
-    //         }
+            try {
+                Swal.fire(
+                    `Vé hợp lệ! ${data.text}`,
+                    '',
+                    'success'
+                ).then(e => setScanSuccess(false));
+            }catch(e) {
+                console.log(e);
+                Swal.fire(
+                    'Vé không hợp lệ!',
+                    '',
+                    'error'
+                ).then(e => setScanSuccess(false));
+            }
 
            
-    //     };
-    // }
+        };
+    }
 
-    // const handleErr = (err) => {
-    //     console.log(err);
-    // }
+    const handleErr = (err) => {
+        console.log(err);
+    }
 
-    // const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-
-    // let facingMode;
-
-    // if (isIOS) {
-    // facingMode = 'environment';
-    // } else {
-    // facingMode = 'user';
-    // }
-
-
-    // Lấy camera sau trên IOS
     const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-    const facingMode = isIOS ? FacingMode.Environment : FacingMode.User;
 
-    
-    const handleTakePhoto = (dataUri) => {
-        // Làm gì đó với ảnh chụp được
-        Swal.fire(
-            `Vé hợp lệ! ${dataUri}`,
-            '',
-            'success'
-        ).then(e => setScanSuccess(false));
+    let facingMode;
 
-      }
+    if (isIOS) {
+    facingMode = 'environment';
+    } else {
+    facingMode = 'user';
+    }
+
 
   return (
     <Section className={`min-h-screen flex flex-col gap-4 flex justify-center items-center`}>     
-        <h1>SCAN QR V2</h1>  
+        <h1>SCAN QR V1</h1>  
         <div className='w-[full] h-[30rem] overflow-hidden'>
-            {/* <QrReader
+            <QrReader
                 delay={ 100 }
                 style={{
                     width: '100%',
@@ -74,12 +57,7 @@ const Scan = () => {
                 onError={ handleErr }
                 onScan={ handleScan }
                 facingMode={facingMode}
-            /> */}
-
-<CameraPhoto
-  facingMode={facingMode}
-  onTakePhoto = { (dataUri) => { handleTakePhoto(dataUri); } }  
-/>
+            />
         </div>
     </Section>
   )
